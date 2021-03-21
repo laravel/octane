f7218636f54bb84a4903bea7a88d1681553c26e9
Taylor Otwell <taylorotwell@gmail.com>
2021-03-21 14:17:57 -0500

    wip

--- src/Octane.php
+++ src/Octane.php
@@ -4,7 +4,6 @@ namespace Laravel\Octane;
 
 use Laravel\Octane\Contracts\DispatchesCoroutines;
 use Laravel\Octane\Contracts\DispatchesTasks;
-use Laravel\Octane\SequentialTaskDispatcher;
 use Laravel\Octane\Swoole\SwooleTaskDispatcher;
 use Swoole\Http\Server;
 use Throwable;
