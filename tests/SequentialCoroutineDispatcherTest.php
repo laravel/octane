2cfb52f8d47e8177bc4d9142b6504ee50a47956d
Taylor Otwell <taylorotwell@gmail.com>
2021-03-21 14:14:01 -0500

    wip

---
+++ tests/SequentialCoroutineDispatcherTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Laravel\Octane\Tests;
+
+use Laravel\Octane\SequentialCoroutineDispatcher;
+
+class SequentialCoroutineDispatcherTest extends TestCase
+{
+    /** @test */
+    public function test_coroutines_can_be_resolved()
+    {
+        $dispatcher = new SequentialCoroutineDispatcher;
+
+        $this->assertEquals([
+            'first' => 1,
+            'second' => 2,
+        ], $dispatcher->resolve([
+            'first' => fn () => 1,
+            'second' => fn () => 2,
+        ]));
+    }
+}
---
+++ tests/SequentialTaskDispatcherTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Laravel\Octane\Tests;
+
+use Laravel\Octane\SequentialTaskDispatcher;
+
+class SequentialTaskDispatcherTest extends TestCase
+{
+    /** @test */
+    public function test_tasks_can_be_resolved()
+    {
+        $dispatcher = new SequentialTaskDispatcher;
+
+        $this->assertEquals([
+            'first' => 1,
+            'second' => 2,
+        ], $dispatcher->resolve([
+            'first' => fn () => 1,
+            'second' => fn () => 2,
+        ]));
+    }
+
+    /** @test */
+    public function test_tasks_can_be_dispatched()
+    {
+        $dispatcher = new SequentialTaskDispatcher;
+
+        $first = null;
+        $second = null;
+
+        $dispatcher->dispatch([
+            'first' => function () use (&$first) { $first = 'first'; },
+            'second' => function () use (&$second) { $second = 'second'; },
+        ]);
+
+        $this->assertEquals('first', $first);
+        $this->assertEquals('second', $second);
+    }
+}
