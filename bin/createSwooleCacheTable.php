948ccd672e4551bd6903374b99d9b26a8883eaca
Taylor Otwell <taylorotwell@gmail.com>
2021-03-23 14:04:23 -0700

    wip

--- bin/createSwooleCacheTable.php
+++ bin/createSwooleCacheTable.php
@@ -3,12 +3,12 @@
 use Swoole\Table;
 
 if ($serverState['octaneConfig']['cache'] ?? false) {
-    $table = new Table($serverState['octaneConfig']['cache']['rows'] ?? 1000);
+    $cacheTable = new Table($serverState['octaneConfig']['cache']['rows'] ?? 1000);
 
-    $table->column('value', Table::TYPE_STRING, $serverState['octaneConfig']['cache']['bytes'] ?? 10000);
-    $table->column('expiration', Table::TYPE_INT);
+    $cacheTable->column('value', Table::TYPE_STRING, $serverState['octaneConfig']['cache']['bytes'] ?? 10000);
+    $cacheTable->column('expiration', Table::TYPE_INT);
 
-    $table->create();
+    $cacheTable->create();
 
-    return $table;
+    return $cacheTable;
 }
