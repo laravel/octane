8b1e4d6b49121794a538cfb3535ceb5e7e952356
Taylor Otwell <taylorotwell@gmail.com>
2021-03-24 15:32:38 -0700

    wip

---
+++ src/Concerns/RegistersTickHandlers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Laravel\Octane\Concerns;
+
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Facades\Cache;
+use Laravel\Octane\Events\TickReceived;
+use Laravel\Octane\Swoole\InvokeTickCallable;
+
+trait RegistersTickHandlers
+{
+    /**
+     * Register a callback to be called every N seconds.
+     *
+     * @param  string  $key
+     * @param  callable  $callback
+     * @param  int  $seconds
+     * @param  bool  $immediate
+     * @return void
+     */
+    public function tick(string $key, callable $callback, int $seconds = 1, bool $immediate = true)
+    {
+        app(Dispatcher::class)->listen(
+            TickReceived::class,
+            new InvokeTickCallable(
+                $key,
+                $callback,
+                $seconds,
+                $immediate,
+                Cache::store('octane'), app(ExceptionHandler::class)
+            )
+        );
+    }
+}
--- src/Octane.php
+++ src/Octane.php
@@ -2,10 +2,6 @@
 
 namespace Laravel\Octane;
 
-use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Support\Facades\Cache;
-use Laravel\Octane\Events\TickReceived;
 use Throwable;
 
 class Octane
@@ -13,29 +9,7 @@ class Octane
     use Concerns\ProvidesConcurrencySupport;
     use Concerns\ProvidesDefaultConfigurationOptions;
     use Concerns\ProvidesRouting;
-
-    /**
-     * Register a callback to be called every N seconds.
-     *
-     * @param  string  $key
-     * @param  callable  $callback
-     * @param  int  $seconds
-     * @param  bool  $immediate
-     * @return void
-     */
-    public function tick(string $key, callable $callback, int $seconds = 1, bool $immediate = true)
-    {
-        app(Dispatcher::class)->listen(
-            TickReceived::class,
-            new Swoole\InvokeTickCallable(
-                $key,
-                $callback,
-                $seconds,
-                $immediate,
-                Cache::store('octane'), app(ExceptionHandler::class)
-            )
-        );
-    }
+    use Concerns\RegistersTickHandlers;
 
     /**
      * Format an exception to a string that should be returned to the client.
