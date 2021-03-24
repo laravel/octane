54c525f374190f08b9a3d89294b2d988d67f9edf
Nuno Maduro <enunomaduro@gmail.com>
2021-03-24 23:08:49 +0000

    Adds report exceptions test

---
+++ tests/Listeners/ReportExceptionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Laravel\Octane\Listeners;
+
+use Exception;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Laravel\Octane\Events\WorkerErrorOccurred;
+use Laravel\Octane\Stream;
+use Laravel\Octane\Tests\TestCase;
+use Mockery;
+
+class ReportExceptionTest extends TestCase
+{
+    /** @doesNotPerformAssertions @test */
+    public function test_exceptions_are_streamed()
+    {
+        [$app, $worker] = $this->createOctaneContext([]);
+
+        $exception = new Exception('foo');
+
+        Mockery::mock('alias:'.Stream::class)
+            ->shouldReceive('error')
+            ->once()
+            ->with($exception);
+
+        $worker->dispatchEvent($app, new WorkerErrorOccurred($exception, $app));
+    }
+
+    /** @doesNotPerformAssertions @test */
+    public function test_exceptions_are_reported()
+    {
+        [$app, $worker] = $this->createOctaneContext([]);
+
+        $exception = new Exception('foo');
+
+        $exceptionHandler = tap(Mockery::mock(ExceptionHandler::class), fn ($mock) => $mock
+            ->shouldReceive('report')
+            ->once()
+            ->with($exception));
+
+        Mockery::mock('alias:'.Stream::class)
+            ->shouldReceive('error')
+            ->once()
+            ->with($exception);
+
+        $app->bind(ExceptionHandler::class, fn () => $exceptionHandler);
+
+        $worker->dispatchEvent($app, new WorkerErrorOccurred($exception, $app));
+    }
+}
