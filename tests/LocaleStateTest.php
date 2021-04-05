1b94d4a1867246e810297d4719b9c9953637a1dc
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-06 07:02:26 +0800

    Flush locale state between request
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/LocaleStateTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Laravel\Octane\Tests;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+
+class LocaleStateTest extends TestCase
+{
+    /** @test */
+    public function test_pagination_state_is_updated_across_subsequent_requests()
+    {
+        [$app, $worker, $client] = $this->createOctaneContext([
+            Request::create('/test-locale?locale=nl', 'GET'),
+            Request::create('/test-locale', 'GET'),
+            Request::create('/test-locale?locale=ms', 'GET'),
+        ]);
+
+        $app['router']->get('/test-locale', function (Application $app, Request $request) {
+            if ($request->has('locale')) {
+                $app->setLocale($request->query('locale'));
+            }
+
+            return $app->make('translator')->getLocale();
+        });
+
+        $worker->run();
+
+        $this->assertEquals('nl', $client->responses[0]->getContent());
+        $this->assertEquals('en', $client->responses[1]->getContent());
+        $this->assertEquals('ms', $client->responses[2]->getContent());
+    }
+}
