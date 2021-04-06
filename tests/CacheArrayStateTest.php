6321ea1a25e279852401d0bd45b5daca0905f3b0
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-06 21:13:46 +0800

    Add failing test for array cache should be flush between request. (#64)
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/CacheArrayStateTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Laravel\Octane\Tests;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class CacheArrayStateTest extends TestCase
+{
+    /** @test */
+    public function test_array_cache_is_flushed_between_requests()
+    {
+        [$app, $worker, $client] = $this->createOctaneContext([
+            Request::create('/test-cache?remember=first', 'GET'),
+            Request::create('/test-cache?remember=second', 'GET'),
+        ]);
+
+        $app['router']->middleware('web')->get('/test-cache', function (Application $app, Request $request) {
+            return $app['cache']->driver('array')->rememberForever('nitro', fn () => $request->query('remember'));
+        });
+
+        $worker->run();
+
+        $this->assertEquals('first', $client->responses[0]->getContent());
+        $this->assertEquals('second', $client->responses[1]->getContent());
+    }
+}
