48f0d427b8243069b646f43702e942a784032379
Marvin Mall <marvin-mall@msn.com>
2021-04-06 21:05:00 +0200

    Add methods to facade doc block

--- src/Facades/Octane.php
+++ src/Facades/Octane.php
@@ -5,6 +5,13 @@ namespace Laravel\Octane\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Laravel\Octane\Swoole\InvokeTickCallable tick(string $key, callable $callback, int $seconds = 1, bool $immediate = true)
+ * @method static \Swoole\Table table(string $name)
+ * @method static \Symfony\Component\HttpFoundation\Response invokeRoute(\Illuminate\Http\Request $request, string $method, string $uri)
+ * @method static array concurrently(array $tasks, int $waitMilliseconds = 3000)
+ * @method static bool hasRouteFor(string $method, string $uri)
+ * @method static void route(string $method, string $uri, \Closure $callback)
+ * 
  * @see \Laravel\Octane\Octane
  */
 class Octane extends Facade
