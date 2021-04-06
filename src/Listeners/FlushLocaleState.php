b665e12e247ec57d5965cd70f5a1f27903971983
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-06 21:31:36 +0800

    Flush locale state between request (#63)
    * Flush locale state between request
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * update test name
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * reset value from config.
    
    * Apply fixes from StyleCI (#75)
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

---
+++ src/Listeners/FlushLocaleState.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Laravel\Octane\Listeners;
+
+class FlushLocaleState
+{
+    /**
+     * Handle the event.
+     *
+     * @param  mixed  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $config = $event->sandbox->make('config');
+
+        tap($event->sandbox->make('translator'), function ($translator) use ($config) {
+            $translator->setLocale($config->get('app.locale'));
+            $translator->setFallback($config->get('app.fallback_locale'));
+        });
+    }
+}
