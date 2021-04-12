c620e8bb99e2b479c043c7507739383f3fa6ef18
Mohamed Samir <gm.mohamedsamir@gmail.com>
2021-04-12 15:52:19 +0200

    Allow reuse the octane cache after stop the swoole server

--- src/Swoole/Handlers/OnManagerStart.php
+++ src/Swoole/Handlers/OnManagerStart.php
@@ -2,13 +2,18 @@
 
 namespace Laravel\Octane\Swoole\Handlers;
 
+use Illuminate\Support\Facades\Cache;
+use Laravel\Octane\ApplicationFactory;
 use Laravel\Octane\Swoole\SwooleExtension;
+use Laravel\Octane\Swoole\WorkerState;
 
 class OnManagerStart
 {
     public function __construct(
         protected SwooleExtension $extension,
-        protected string $appName,
+        protected array $serverState,
+        protected mixed $bootstrap,
+        protected WorkerState $workerState,
         protected bool $shouldSetProcessName = true
     ) {
     }
@@ -21,7 +26,29 @@ class OnManagerStart
     public function __invoke()
     {
         if ($this->shouldSetProcessName) {
-            $this->extension->setProcessName($this->appName, 'manager process');
+            $this->extension->setProcessName($this->serverState['appName'], 'manager process');
         }
+
+        $this->restoreOctaneCache(($this->bootstrap)($this->serverState));
+    }
+
+    /**
+     * Restore the octane cache that we stored on "managerstop" event.
+     *
+     * @param  string $basePath
+     * @return void
+     */
+    protected function restoreOctaneCache(string $basePath)
+    {
+        $tmpApp = new ApplicationFactory($basePath);
+        $tmpApp->createApplication();
+
+        $cache = (array) Cache::get("octane-cache");
+
+        foreach ($cache as $key => $row) {
+            $this->workerState->cacheTable[$key] = $row;
+        }
+
+        unset($tmpApp, $cache);
     }
 }
---
+++ src/Swoole/Handlers/OnManagerStop.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Laravel\Octane\Swoole\Handlers;
+
+use Exception;
+use Illuminate\Support\Facades\Cache;
+use Laravel\Octane\ApplicationFactory;
+use Laravel\Octane\Swoole\WorkerState;
+
+class OnManagerStop
+{
+    public function __construct(
+        protected string $basePath,
+        protected WorkerState $workerState
+    ) {
+    }
+
+    /**
+     * Handle the "managerstop" Swoole event.
+     *
+     * @return void
+     */
+    public function __invoke()
+    {
+        $tmpApp = new ApplicationFactory($this->basePath);
+        $tmpApp->createApplication();
+
+        $cache = [];
+
+        foreach ($this->workerState->cacheTable as $key => $row) {
+            $cache[$key] = $row;
+        }
+
+        Cache::put("octane-cache", $cache);
+
+        unset($tmpApp, $cache);
+    }
+}
