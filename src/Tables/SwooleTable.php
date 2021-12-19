f9a1e2063748b6a6b8f742cba8d3134beefb683e
sy-records <52o@qq52o.cn>
2021-12-19 11:15:23 +0800

    Support type hint for Swoole 4.8

--- src/Tables/SwooleTable.php
+++ src/Tables/SwooleTable.php
@@ -4,44 +4,88 @@ namespace Laravel\Octane\Tables;
 
 use Swoole\Table;
 
-class SwooleTable extends Table
-{
-    use Concerns\EnsuresColumnSizes;
-
-    /**
-     * The table columns.
-     *
-     * @var array
-     */
-    protected $columns;
-
-    /**
-     * Set the data type and size of the columns.
-     *
-     * @param  string  $name
-     * @param  int  $type
-     * @param  int  $size
-     * @return bool
-     */
-    public function column(string $name, int $type, int $size = 0): bool
+if (PHP_VERSION_ID > 80000 && SWOOLE_VERSION_ID >= 40840) {
+    class SwooleTable extends Table
     {
-        $this->columns[$name] = [$type, $size];
+        use Concerns\EnsuresColumnSizes;
 
-        return parent::column($name, $type, $size);
+        /**
+         * The table columns.
+         *
+         * @var array
+         */
+        protected $columns;
+
+        /**
+         * Set the data type and size of the columns.
+         *
+         * @param  string  $name
+         * @param  int  $type
+         * @param  int  $size
+         * @return bool
+         */
+        public function column(string $name, int $type, int $size = 0): bool
+        {
+            $this->columns[$name] = [$type, $size];
+
+            return parent::column($name, $type, $size);
+        }
+
+        /**
+         * Update a row of the table.
+         *
+         * @param  string  $key
+         * @param  array  $values
+         * @return bool
+         */
+        public function set(string $key, array $values): bool
+        {
+            collect($values)
+                ->each($this->ensureColumnsSize());
+
+            return parent::set($key, $values);
+        }
     }
-
-    /**
-     * Update a row of the table.
-     *
-     * @param  string  $key
-     * @param  array  $values
-     * @return bool
-     */
-    public function set(string $key, array $values): bool
+} else {
+    class SwooleTable extends Table
     {
-        collect($values)
-            ->each($this->ensureColumnsSize());
+        use Concerns\EnsuresColumnSizes;
 
-        return parent::set($key, $values);
+        /**
+         * The table columns.
+         *
+         * @var array
+         */
+        protected $columns;
+
+        /**
+         * Set the data type and size of the columns.
+         *
+         * @param  string  $name
+         * @param  int  $type
+         * @param  int  $size
+         * @return void
+         */
+        public function column($name, $type, $size = 0)
+        {
+            $this->columns[$name] = [$type, $size];
+
+            parent::column($name, $type, $size);
+        }
+
+        /**
+         * Update a row of the table.
+         *
+         * @param  string  $key
+         * @param  array  $values
+         * @return void
+         */
+        public function set($key, array $values)
+        {
+            collect($values)
+                ->each($this->ensureColumnsSize());
+
+            parent::set($key, $values);
+        }
     }
 }
