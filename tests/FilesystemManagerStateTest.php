d3795c6d163b5e324095fa4fdfb6cda73fbb4204
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-01-06 13:20:50 +0100

    Fix FilesystemManager app instance

---
+++ tests/FilesystemManagerStateTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laravel\Octane\Tests;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use ReflectionProperty;
+
+class FilesystemManagerStateTest extends TestCase
+{
+    public function test_filesystem_manager_has_fresh_application_instance()
+    {
+        [$app, $worker, $client] = $this->createOctaneContext([
+            Request::create('/first', 'GET'),
+            Request::create('/first', 'GET'),
+        ]);
+
+        $filesystemManagerApplication = new ReflectionProperty($app['filesystem'], 'app');
+
+        $app['router']->get('/first', function (Application $app) use ($filesystemManagerApplication) {
+            return spl_object_hash($filesystemManagerApplication->getValue($app['filesystem']));
+        });
+
+        $worker->run();
+
+        $this->assertNotEquals($client->responses[0]->original, $client->responses[1]->original);
+    }
+}
