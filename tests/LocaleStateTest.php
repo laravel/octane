645a21205fd74f0aeee8810b0eaa47a4237b0dda
Nuno Maduro <enunomaduro@gmail.com>
2022-07-15 19:05:10 +0100

    Fixes shared Carbon Locale between requests

--- tests/LocaleStateTest.php
+++ tests/LocaleStateTest.php
@@ -4,6 +4,7 @@ namespace Laravel\Octane\Tests;
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class LocaleStateTest extends TestCase
 {
@@ -28,5 +29,28 @@ class LocaleStateTest extends TestCase
         $this->assertEquals('nl', $client->responses[0]->getContent());
         $this->assertEquals('en', $client->responses[1]->getContent());
         $this->assertEquals('ms', $client->responses[2]->getContent());
+    }
+
+    public function test_carbon_state_is_reset_across_subsequent_requests()
+    {
+        [$app, $worker, $client] = $this->createOctaneContext([
+            Request::create('/test-locale', 'GET'), // should be "en"
+            Request::create('/test-locale?locale=nl', 'GET'),
+            Request::create('/test-locale', 'GET'), // should be "en", and not "nl"...
+        ]);
+
+        $app['router']->get('/test-locale', function (Application $app, Request $request) {
+            if ($request->has('locale')) {
+                Carbon::setLocale($request->query('locale'));
+            }
+
+            return now()->getLocale();
+        });
+
+        $worker->run();
+
+        $this->assertEquals('en', $client->responses[0]->getContent());
+        $this->assertEquals('nl', $client->responses[1]->getContent());
+        $this->assertEquals('en', $client->responses[2]->getContent());
     }
 }
