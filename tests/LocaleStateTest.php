d95f37fc272d26bbbd2a432898c4c3c5974645e2
Nuno Maduro <enunomaduro@gmail.com>
2022-07-25 15:11:08 +0100

    Fix carbon locale when setting it via app locale setter

--- tests/LocaleStateTest.php
+++ tests/LocaleStateTest.php
@@ -53,4 +53,29 @@ class LocaleStateTest extends TestCase
         $this->assertEquals('nl', $client->responses[1]->getContent());
         $this->assertEquals('en', $client->responses[2]->getContent());
     }
+
+    public function test_carbon_and_app_locale_gets_updated_when_changing_app_locale()
+    {
+        [$app, $worker, $client] = $this->createOctaneContext([
+            Request::create('/test-locale', 'GET'), // should be "en"
+            Request::create('/test-locale?locale=nl', 'GET'), // should be "nl"
+            Request::create('/test-locale', 'GET'), // should be "en", and not "nl"
+            Request::create('/test-locale?locale=pt', 'GET'), // should be "pt"
+        ]);
+
+        $app['router']->get('/test-locale', function (Application $app, Request $request) {
+            if ($request->has('locale')) {
+                app()->setLocale($request->query('locale'));
+            }
+
+            return [app()->getLocale(), now()->getLocale()];
+        });
+
+        $worker->run();
+
+        $this->assertEquals(['en', 'en'], json_decode($client->responses[0]->getContent(), true));
+        $this->assertEquals(['nl', 'nl'], json_decode($client->responses[1]->getContent(), true));
+        $this->assertEquals(['en', 'en'], json_decode($client->responses[2]->getContent(), true));
+        $this->assertEquals(['pt', 'pt'], json_decode($client->responses[3]->getContent(), true));
+    }
 }
