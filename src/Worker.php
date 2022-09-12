beab7afa2354eca7b93e312e64ff8dc8cec76dc4
hamid <47260068+zack7z@users.noreply.github.com>
2022-09-12 13:40:19 +0430

    handle ob_end_clean() issue
    added a condition to prevent "ob_end_clean(): Failed to delete buffer. No buffer to delete" error in some situation (in my case ckfinder)

--- src/Worker.php
+++ src/Worker.php
@@ -92,8 +92,10 @@ class Worker implements WorkerContract
             $response = $gateway->handle($request);
 
             $output = ob_get_contents();
-
-            ob_end_clean();
+            
+            if ($output) {
+                ob_end_clean();
+            }
 
             // Here we will actually hand the incoming request to the Laravel application so
             // it can generate a response. We'll send this response back to the client so
