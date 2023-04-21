34c2aeb80a3dcaf88c6823e282ee7222b0f986b3
NiroDeveloper <23154768+NiroDeveloper@users.noreply.github.com>
2023-04-21 16:15:12 +0200

    Reverted ide phpdoc styling on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

--- src/Swoole/SwooleClient.php
+++ src/Swoole/SwooleClient.php
@@ -171,8 +171,8 @@ class SwooleClient implements Client, ServesStaticFiles
     /**
      * Send the headers from the Illuminate response to the Swoole response.
      *
-     * @param \Symfony\Component\HttpFoundation\Response $response
--    * @param \Swoole\Http\Response $response
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @param  \Swoole\Http\Response  $response
      * @return void
      */
     public function sendResponseHeaders(Response $response, SwooleResponse $swooleResponse): void
