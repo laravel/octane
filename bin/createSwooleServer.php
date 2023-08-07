8685ef386f0e94a8190eb907ada1d3e01351aff6
Taylor Otwell <taylor@laravel.com>
2023-08-07 08:05:49 -0500

    Update createSwooleServer.php

--- bin/createSwooleServer.php
+++ bin/createSwooleServer.php
@@ -4,7 +4,9 @@ $config = $serverState['octaneConfig'];
 
 try {
     $host = $serverState['host'] ?? '127.0.0.1';
+
     $sock = filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ? SWOOLE_SOCK_TCP : SWOOLE_SOCK_TCP6;
+
     $server = new Swoole\Http\Server(
         $host,
         $serverState['port'] ?? 8080,
