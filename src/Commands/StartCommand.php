82088b7aa769fcb35d3a336721a4dc50b9e1143f
Taylor Otwell <taylor@laravel.com>
2023-12-13 14:22:01 -0600

    formatting

--- src/Commands/StartCommand.php
+++ src/Commands/StartCommand.php
@@ -24,7 +24,7 @@ class StartCommand extends Command implements SignalableCommandInterface
                     {--max-requests=500 : The number of requests to process before reloading the server}
                     {--rr-config= : The path to the RoadRunner .rr.yaml file}
                     {--frankenphp-config= : The path to the FrankenPHP Caddyfile file}
-                    {--https : Enable HTTPS, HTTP/2 and HTTP/3, automatically generate and renew certificates}
+                    {--https : Enable HTTPS, HTTP/2, and HTTP/3, automatically generate and renew certificates [FrankenPHP only]}
                     {--watch : Automatically reload the server when the application is modified}
                     {--poll : Use file system polling while watching in order to watch files over a network}
                     {--log-level= : Log messages at or above the specified log level}';
