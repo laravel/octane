af34f43e00618ee66880a2d947bd5caa176ec519
Nuno Maduro <enunomaduro@gmail.com>
2023-12-15 11:36:23 +0000

    Removes non-needed comments

--- tests/FrankenPhpServerStateFileTest.php
+++ tests/FrankenPhpServerStateFileTest.php
@@ -15,17 +15,14 @@ class FrankenPhpServerStateFileTest extends TestCase
 
         $stateFile->delete();
 
-        // Read file...
         $state = $stateFile->read();
         $this->assertEquals(['masterProcessId' => null, 'state' => []], $state);
 
-        // Write file...
         $stateFile->writeProcessId(1);
         $stateFile->writeState(['name' => 'Taylor']);
         $state = $stateFile->read();
         $this->assertEquals(['masterProcessId' => 1, 'state' => ['name' => 'Taylor']], $state);
 
-        // Delete file...
         $stateFile->delete();
         $state = $stateFile->read();
         $this->assertEquals(['masterProcessId' => null, 'state' => []], $state);
