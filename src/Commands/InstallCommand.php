2dd3c21d054b525127563270e68a26dbc1124535
Taylor Otwell <taylor@laravel.com>
2023-12-18 09:58:55 -0600

    Update InstallCommand.php

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -125,7 +125,7 @@ class InstallCommand extends Command
      */
     public function installFrankenPhpServer()
     {
-        if (! $this->confirm("FrankenPHP's Octane integration is in beta and should used with caution in production. Do you wish to continue?")) {
+        if (! $this->confirm("FrankenPHP's Octane integration is in beta and should be used with caution in production. Do you wish to continue?")) {
             return false;
         }
 
