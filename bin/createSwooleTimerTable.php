17e090abb91b42fa5b303e84f6a4549813d14590
miguilim <35383529+srdante@users.noreply.github.com>
2024-01-26 23:20:14 -0300

    Use workers count to timer table size instead of 250 or param

--- bin/createSwooleTimerTable.php
+++ bin/createSwooleTimerTable.php
@@ -6,7 +6,7 @@ use Swoole\Table;
 require_once __DIR__.'/../src/Tables/TableFactory.php';
 
 if (($serverState['octaneConfig']['max_execution_time'] ?? 0) > 0) {
-    $timerTable = TableFactory::make($serverState['octaneConfig']['max_timer_table_size'] ?? 250);
+    $timerTable = TableFactory::make($serverState['workers']);
 
     $timerTable->column('worker_pid', Table::TYPE_INT);
     $timerTable->column('time', Table::TYPE_INT);
