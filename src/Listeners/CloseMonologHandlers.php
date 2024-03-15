a54a8b8ba567abd4de1bbcfe095760e7b5c9db33
NiroDeveloper <NiroDeveloper@users.noreply.github.com>
2024-03-15 08:53:23 +0000

    Fix code styling

--- src/Listeners/CloseMonologHandlers.php
+++ src/Listeners/CloseMonologHandlers.php
@@ -4,17 +4,15 @@ namespace Laravel\Octane\Listeners;
 
 class CloseMonologHandlers
 {
-
     public function handle($event): void
     {
-        if (!$event->app->resolved('log')) {
+        if (! $event->app->resolved('log')) {
             return;
         }
 
         collect($event->app->make('log')->getChannels())
             ->map->getLogger()
-            ->filter(fn($logger) => method_exists($logger, 'close'))
+            ->filter(fn ($logger) => method_exists($logger, 'close'))
             ->each->close();
     }
-
 }
