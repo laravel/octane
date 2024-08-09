504dd7f0638a4575003ddb88d8a63b0813155502
hugh.li <hugh.li@foxmail.com>
2024-08-09 11:46:48 +0800

    Do not perform deletion operations during traversal (all keys can be taken out for deletion after traversal)
    Do not perform deletion operations during traversal (all keys can be taken out for deletion after traversal)

--- src/Swoole/Actions/EnsureRequestsDontExceedMaxExecutionTime.php
+++ src/Swoole/Actions/EnsureRequestsDontExceedMaxExecutionTime.php
@@ -23,23 +23,31 @@ class EnsureRequestsDontExceedMaxExecutionTime
      */
     public function __invoke()
     {
+        $rows = [];
         foreach ($this->timerTable as $workerId => $row) {
             if ((time() - $row['time']) > $this->maxExecutionTime) {
-                $this->timerTable->del($workerId);
+                $rows[$workerId] = $row;
+            }
+        }
 
-                if ($this->server instanceof Server && ! $this->server->exists($row['fd'])) {
-                    continue;
-                }
 
-                $this->extension->dispatchProcessSignal($row['worker_pid'], SIGKILL);
+        foreach ($rows as $workerId => $row) {
+            if ($this->timerTable->get($workerId, 'fd') !== $row['fd']) {
+                continue;
+            }
 
-                if ($this->server instanceof Server) {
-                    $response = Response::create($this->server, $row['fd']);
+            $this->timerTable->del($workerId);
+            if ($this->server instanceof Server && !$this->server->exists($row['fd'])) {
+                continue;
+            }
 
-                    if ($response) {
-                        $response->status(408);
-                        $response->end();
-                    }
+            $this->extension->dispatchProcessSignal($row['worker_pid'], SIGKILL);
+
+            if ($this->server instanceof Server) {
+                $response = Response::create($this->server, $row['fd']);
+                if ($response) {
+                    $response->status(408);
+                    $response->end();
                 }
             }
         }
