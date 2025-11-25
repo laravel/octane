93b749d121721cd57998d0dcd200f9e139cd28b8
Mior Muhammad Zaki <crynobone@gmail.com>
2025-11-25 19:59:40 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/OnWorkerStartTest.php
+++ tests/OnWorkerStartTest.php
@@ -18,7 +18,6 @@ class OnWorkerStartTest extends TestCase
 
         $reflection = new \ReflectionClass($handler);
         $method = $reflection->getMethod('shouldClearOpcodeCache');
-        $method->setAccessible(true);
 
         $this->assertTrue($method->invoke($handler));
     }
@@ -43,7 +42,6 @@ class OnWorkerStartTest extends TestCase
 
         $reflection = new \ReflectionClass($handler);
         $method = $reflection->getMethod('shouldClearOpcodeCache');
-        $method->setAccessible(true);
 
         $this->assertFalse($method->invoke($handler));
     }
