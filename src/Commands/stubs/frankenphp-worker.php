ae618600cb54826a21f67d130a39446f68be1a9a
Kieran Brown <kswb96@gmail.com>
2025-12-21 23:59:50 +0000

    Fix APP_PUBLIC_PATH fallback logic in frankenphp-worker (#1085)

--- src/Commands/stubs/frankenphp-worker.php
+++ src/Commands/stubs/frankenphp-worker.php
@@ -2,6 +2,6 @@
 
 // Set a default for the application base path and public path if they are missing...
 $_SERVER['APP_BASE_PATH'] = $_ENV['APP_BASE_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__.'/..';
-$_SERVER['APP_PUBLIC_PATH'] = $_ENV['APP_PUBLIC_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__;
+$_SERVER['APP_PUBLIC_PATH'] = $_ENV['APP_PUBLIC_PATH'] ?? $_SERVER['APP_PUBLIC_PATH'] ?? __DIR__;
 
 require __DIR__.'/../vendor/laravel/octane/bin/frankenphp-worker.php';
