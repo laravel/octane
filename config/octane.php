54141b3b3f2983120a15a69b7558506430a0db95
Jānis Lācis <taai4ce@gmail.com>
2026-02-12 16:52:00 +0200

    Update octane.php

--- config/octane.php
+++ config/octane.php
@@ -221,4 +221,17 @@ return [
 
     'max_execution_time' => 30,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Extra Environment Vairables
+    |--------------------------------------------------------------------------
+    |
+    | Extra environment variables passed to Caddyfile.
+    |
+    */
+
+    'extra_envs' => [
+        // 'SOME_CONFIG_VALUE' => env('SOME_CONFIG_VALUE'),
+    ],
+
 ];
