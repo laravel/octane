d50a6b4d5bdc7c3dc474b6f6c444d27932b978d0
Eduardo Agostini <eduardo@nottaboss.co.nz>
2026-04-16 14:18:46 +1200

    [2.x] Restore Glob Support For Chokidar v5
    The previous implementation of the file-watcher had a version-specific check that broke compatibility with the recently released Chokidar v5. This commit improves the version detection to apply the manual glob polyfill for all versions greater than 3. It also refactors the path and extension parsing to be simpler, avoiding running unnecessarily and ensuring Octane's wildcard configurations continue to function across modern Chokidar releases.

--- bin/file-watcher.cjs
+++ bin/file-watcher.cjs
@@ -1,52 +1,54 @@
 const chokidar = require('chokidar');
 
-const paths = JSON.parse(process.argv[2]);
-const poll = process.argv[3] ? true : false;
-
-// Chokidar removed support for glob in version 4...
-const chokidarPackagePath = require.resolve('chokidar').replace('index.js', 'package.json');
-const chokidarVersion4 = require(chokidarPackagePath).version.startsWith('4.');
-
-const extractWildcardExtension = (path) => {
-    // Match patterns like *.php, **/*.blade.php
-    const match = path.match(/\/\*\.((\w|\.)+)$/);
-
-    return match ? match[1] : null;
-};
-
-const getPathBeforeWildcard = (path) => {
-    const match = path.match(/^(.*?)(\*|$)/);
-
-    return match ? match[1] : path;
-};
-
-const extractedPaths = paths.map(path => {
-    return { path: getPathBeforeWildcard(path), extension: extractWildcardExtension(path) };
-});
-
-const watcherPaths = chokidarVersion4 ? extractedPaths.map(ep => ep.path) : paths;
-
-const watcherIgnored = chokidarVersion4 ? (path, stats) => {
-    if (! stats?.isFile()) {
-        return false;
-    }
-
-    const matchedPattern = extractedPaths.find(ep => path.startsWith(ep.path));
-
-    if (! matchedPattern) {
-        return true;
-    }
-
-    return matchedPattern.extension ? ! path.endsWith(`.${matchedPattern.extension}`) : false;
-} : undefined;
-
-const watcher = chokidar.watch(watcherPaths, {
+const options = {
     ignoreInitial: true,
-    usePolling: poll,
-    ignored: watcherIgnored,
-});
+    paths: JSON.parse(process.argv[2]),
+    usePolling: process.argv[3] ? true : false,
+};
 
-watcher
+/**
+ * Chokidar +4 removed built-in glob support. For users on these versions,
+ * we manually parse the paths to extract base directories and extensions,
+ * ensuring Octane's wildcard configurations continue to function via
+ * the manual "ignored" filter below.
+ */
+const chokidarMajorVersion = () => {
+    const path = require
+        .resolve('chokidar')
+        .replace('index.js', 'package.json');
+
+    return parseInt(require(path)
+        .version
+        .split('.')[0]
+    );
+};
+
+if (chokidarMajorVersion() > 3) {
+    const extractedPaths = options.paths.map(path => ({
+        path: path.match(/^(?<path>.*?)(?=\*|$)/)?.groups?.path ?? path,
+        extension: path.match(/^.+?(?<=\.)(?<extension>.+)/)?.groups?.extension ?? null,
+    }));
+
+    options.ignored = (path, stats) => {
+        if (! stats?.isFile()) {
+            return false;
+        }
+
+        const matchedPattern = extractedPaths.find(ep => path.startsWith(ep.path));
+
+        if (! matchedPattern) {
+            return true;
+        }
+
+        return matchedPattern.extension ? !path.endsWith(`.${matchedPattern.extension}`) : false;
+    };
+
+    options.paths = extractedPaths.map(ep => ep.path);
+}
+
+const { paths: watcherPaths, ...watcherOptions } = options;
+
+chokidar.watch(watcherPaths, watcherOptions)
     .on('add', () => console.log('File added...'))
     .on('change', () => console.log('File changed...'))
     .on('unlink', () => console.log('File deleted...'))
